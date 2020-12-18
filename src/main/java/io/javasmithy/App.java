f5e968451b76ae58abd1688b45df94320d2757f9
phydesmith <peter.k.hydesmith@gmail.com>
2020-12-18 14:33:46 -0600

    added starter jfx items

---
+++ src/main/java/io/javasmithy/App.java
@@ -0,0 +1,27 @@
+package io.javasmithy;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Weather");
+        BorderPane pane = new BorderPane();
+        label.setAlignment(Pos.CENTER);
+        pane.getChildren().add(label);
+        stage.setScene(new Scene(  pane,800, 800));
+        stage.setTitle("Weather App");
+        stage.show();
+    }
+
+}
